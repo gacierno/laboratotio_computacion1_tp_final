b50cee9fd7d498454b7a36b561ad2996aeeb78e2
GasparErrobidart <podmaster@hotmail.com>
2018-05-30 11:49:50 -0300

    Menu

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include "menu/menu.c"
 
+// DEFINE MAIN MENU
+Menu createMainMenu( Menu *m ){
+  m.title = "Main Menu";
+
+  MenuOption clientOption;
+  MenuOption productsOption;
+
+  clientOption.text       = "Clients";
+  productsOption.text     = "Products";
+  clientOption.function   = setMenu("clients");
+  productsOption.function = setMenu("products");
+
+  m.options = [
+    clientOption,
+    productsOption
+  ];
+}
+
 int main(){
 
-  
+  Menu mainMenu;
+  mainMenu = createMainMenu(&mainMenu);
 
   return 0;
 }
