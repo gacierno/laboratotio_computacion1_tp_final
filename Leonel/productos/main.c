a227d8d8c3ce900a6f73a3267320e718e5fac61e
leonelmdq <leonel_473@hotmail.com>
2018-06-08 22:16:32 -0300

    NUEVO
    8/16

--- Leonel/productos/main.c
+++ Leonel/productos/main.c
@@ -2,8 +2,194 @@
 #include <stdlib.h>
 #include <string.h>
 
+// NUEVO //////////////////////
+
 
 typedef struct{
+    int id;
+    char producto[20];
+    int stock;
+    float costo;
+    float pvp;
+    char baja; //‘s’ activo o ‘n’ inactivo
+}Producto;
+
+
+Producto nuevoProducto(void)
+{
+        Producto a;
+        a.id=1 + contarRegistros("productos.bin");
+        printf("La ID del producto es: %i\n",a.id);
+        printf("Ingrese nombre del producto\n");
+        scanf("%s",&a.producto);
+        printf("Ingrese cantidad de productos\n");
+        scanf("%i",&a.stock);
+        printf("Ingrese costo del producto\n");
+        scanf("%f",&a.costo);
+        printf("Ingrese el precio de venta del producto\n");
+        scanf("%f",&a.pvp);
+        printf("El producto esta activo S/N\n");
+        fflush(stdin);
+        scanf("%c",&a.baja);
+        return a;
+}
+
+int contarRegistros( char ar[] )
+{
+    //abrir el archivo lecctura
+    FILE *arch;
+    arch= fopen(ar,"rb");
+    int pos=0,cant=0;
+    if(arch == NULL)
+    {
+        printf("El archivo no existe\n");
+    }
+    else
+    {
+      //ir hasta el final
+      pos=fseek(arch,0,SEEK_END);
+    //contar cuanto bytes
+    //dividirlo por el tamanio del registro
+      cant=pos/sizeof(Producto);
+    }
+    fclose(arch);
+    //dvolver el numero
+    return cant;
+
+
+
+}
+
+void compararProductos(Producto *a,Producto *aux)
+{
+    char control;
+
+    while ( (*a).id == (*aux).id )
+    {
+        printf("La id esta repetida, modificala \n");
+        fflush(stdin);
+        scanf("%c",&control);
+        if(control=='s')
+        {
+            printf("Ingrese una nueva ID\n");
+            scanf("%i",a->id);
+        }
+    }
+    while (strcmp( (*a).producto,(*aux).producto )== 0)
+    {
+        printf("El nombre del producto esta repetido, desea modificarlo? s/n\n");
+        fflush(stdin);
+        scanf("%c",&control);
+        if(control=='s')
+        {
+            printf("Ingrese un nombre nuevo para su producto\n");
+            scanf("%s",a->producto);
+        }
+    }
+
+}
+
+
+void buscarProductos(char ar[], Producto a )
+{
+    FILE *arch;
+    arch = fopen(ar,"rb");
+    Producto aux;
+    if ( arch == NULL)
+    {
+        printf("El archivo no existe\n");
+    }
+    else
+    {
+        while(!feof(arch))
+        {
+            fread(&aux,sizeof(Producto),1,arch);
+            compararProductos(&a,&aux);
+        }
+    }
+    fclose(arch);
+}
+void guardarArchivo(char ar[],Producto a)
+{
+    FILE *arch;
+    arch= fopen(ar,"ab");
+    buscarProductos(ar,a);
+    fwrite(&a,sizeof(Producto),1,arch);
+    fclose(arch);
+}
+int buscarPorID(char ar[], Producto a )
+{
+    //abrir el archivo en lectura
+    //recorrer el archivo
+    //comparar el producto a con el aux
+
+    //comparar el producto a pasado por parametro con el aux
+    //aux es el producto leido del archivo
+    //si son iguales retornar la pos
+    //si no encuentra retornar -1
+
+    FILE *arch;
+    arch = fopen(ar,"rb");
+    Producto aux;
+    int flag= -1;
+    if ( arch == NULL)
+    {
+        printf("El archivo no existe\n");
+    }
+    else
+    {
+        while(!feof(arch) && flag==-1)
+        {
+
+            fread(&aux,sizeof(Producto),1,arch);
+            if(a.id == aux.id )
+            {
+
+                flag=a.id;
+            }
+        }
+    }
+
+    fclose(arch);
+    return flag;
+}
+void bajaProducto(char ar[],int pos)
+{
+    FILE *arch;
+    Producto aux
+    arch=fopen(ar, "r+b");
+    if(pos<1)
+    {
+        printf("No se encontro el producto\n");
+    }
+    else
+    {
+        fseek(arch,sizeof(Producto)*pos-1,SEEK_SET);
+        fread(&aux,sizeof(Producto),1,arch);
+        aux.baja='n';
+        fseek(arch,sizeof(Producto)*pos-1,SEEK_SET);
+        fwrite(&aux,sizeof(Producto),1,arch);
+    }
+    fclose(arch);
+
+}
+int main()
+{
+    Producto nuevo;
+    char palabra[]="productos.bin";
+    nuevo = nuevoProducto();
+    guardarArchivo(palabra,nuevo);
+    printf("%i",sizeof(Producto));
+    return 0;
+}
+
+
+
+
+
+//  VIEJO //////////////////////
+
+/*typedef struct{
     int id;
     char producto[20];
     int stock;
@@ -154,3 +340,4 @@ int main()
     printf("prueba\n\n");
     return 0;
 }
+*/
