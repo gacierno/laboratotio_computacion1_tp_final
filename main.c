3e4a3b26810b1d0d3eb03c1eed8af1014ea02b27
German <gacierno@gmail.com>
2018-06-20 15:07:03 -0300

    main update

--- main.c
+++ main.c
@@ -1,25 +1,9 @@
 #include "./menu/constructor.c"
 
 int main(){
-  constructNavigation();
-  MAIN_MENU.show(&MAIN_MENU);
-  return 0;
+    constructNavigation();
+    MAIN_MENU.show(&MAIN_MENU);
+    return 0;
 }
 
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include "./clientes/clientes.c"
-//
-// int main(){
-  // createClients();
-  //
-  // Cliente last = getLastClient();
-  // Cliente test = getClientByDNI(37030992);
-  //
-  // updateClient(&test,last.dni);
-  // showAllClients();
 
-  // printClient(disableClient(37030992));
-
-//   return 0;
-// }
